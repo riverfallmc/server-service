0a3a33fd2895ea966e101080e0844365aa9268ef
smokingplaya <smokingplaya@yandex.ru>
2025-02-25 03:55:57 +0300

    0.1.2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use adjust::{controllers, controller::Controller, database::{postgres::Postgres, Pool}, main, service::Service};
 use controller::{server::ServerController, client::ClientController, monitoring::MonitoringController};
+use service::{client::ClientService, server::ServerService};
 
 mod repository;
 mod controller;
@@ -14,38 +15,21 @@ pub struct AppState {
   postgres: Arc<Pool<Postgres>>
 }
 
-// #[tokio::main]
-// async fn main() -> HttpResult<()> {
-//   setup()?;
-
-//   let state = AppState {
-//     postgres: establish_connection()?
-//   };
-
-//   // TODO вообще ряльно не оч красиво что оно тут загружается, но ладно
-//   ClientService::load_client_list(&mut state.postgres.get()?)
-//     .await?;
-
-//   ServerService::load_server_list(&mut state.postgres.get()?)
-//     .await?;
-
-//   let router = Router::new()
-//     .apply_controller(ServerController)
-//     .apply_controller(ClientController)
-//     .apply_controller(MonitoringController)
-//     .with_state(state);
-
-//   let listener = tokio::net::TcpListener::bind("0.0.0.0:80")
-//     .await?;
-
-//   Ok(axum::serve(listener, router).await?)
-// }
-
 #[main]
 async fn main() -> Service<'_, AppState> {
+  let state = AppState::default();
+
+  #[allow(unused)]
+  ClientService::load_client_list(&mut state.postgres.get()?)
+    .await.expect("failed to load client list");
+
+  #[allow(unused)]
+  ServerService::load_server_list(&mut state.postgres.get()?)
+    .await.expect("failed to load client list");
+
   Service {
     name: "Server",
-    state: AppState::default(),
+    state,
     controllers: controllers![ServerController, ClientController, MonitoringController],
     ..Default::default()
   }
